835e0f96fc07d97ad2eab1247b70f290ea5af739
Syed Shaon <53667342+OrionShaon@users.noreply.github.com>
2024-03-19 03:07:30 +0600

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,8 +9,8 @@
                     
                     <div class="page-section mb-60">
                         <div class="container">
-                            <div class="row">
-                                <div class="col-sm-12 col-md-12 col-xs-12 col-lg-6 mb-30">
+                            <div class="row d-flex">
+                                <div class="col-sm-12 col-md-12 col-xs-12 col-lg-6 mb-30 mx-auto">
                                     <!-- Login Form s-->
                                     <form method="POST" action="{{ route('login') }}">
                                        @csrf
