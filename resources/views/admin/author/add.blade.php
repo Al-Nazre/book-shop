a71e9968eb563bea226b9d9ae2c54ef65ea3566d
Syed Shaon <53667342+OrionShaon@users.noreply.github.com>
2024-03-21 23:47:36 +0600

    updated author form

--- resources/views/admin/author/add.blade.php
+++ resources/views/admin/author/add.blade.php
@@ -1,8 +1,12 @@
 @extends('layouts.admin')
 
 @section('content')
+
+<style>
+    
+ </style>
 <div class="pagetitle">
-      <h1>Add Author</h1>
+      <h1 class="text-primary mb-4">Add Author</h1>
       {{-- <nav>
         <ol class="breadcrumb">
           <li class="breadcrumb-item"><a href="index.html">Home</a></li>
@@ -13,55 +17,55 @@
 </div> 
 <section class="section">
       <div class="row">
-        <div class="col-lg-8">
-            <div class="card">
+        <div class="col-lg-6">
+            <div class="card border-primary">
                 <div class="card-body">
-                    <h5 class="card-title" align='center'>Add New Author</h5>
+                    <h5 class="card-title text-success fs-2" align='center'>Add New Author</h5>
 
                     <form action="{{ route('author.insert') }}" method="post" enctype="multipart/form-data">
                         @csrf
-                        <div class="row mb-3">
-                            <label for="inputText" class="col-sm-2 col-form-label">Name</label>
-                            <div class="col-sm-10">
-                                <input type="text" class="form-control" name ='name'>
-                            </div>
+                        <div class="mb-3 form-outline">
+                            <input type="text" class="form-control " name ='name' placeholder="Name">
                         </div>
-                        <div class="row mb-3">
-                            <label for="inputText" class="col-sm-2 col-form-label">Slug</label>
-                            <div class="col-sm-10">
-                                <input type="text" class="form-control" name ='slug'>
-                            </div>
+                        <div class="mb-3">
+                            <input type="text" class="form-control" name ='slug' placeholder ='Slug'>
                         </div>
-                        <div class="row mb-3">
-                            <label for="inputNumber" class="col-sm-2 col-form-label">Image</label>
-                            <div class="col-sm-10">
-                                <input class="form-control" type="file" id="formFile" name="img">
-                            </div>
+                        <div class="mb-3">
+                            <input class="form-control" type="file" id="formFile" name="img" placeholder="Image">
                         </div>
                         
-                        <div class="row mb-3">
-                            <label for="inputPassword" class="col-sm-2 col-form-label">Description</label>
-                            <div class="col-sm-10">
-                                <textarea class="form-control" style="height: 100px" name="description"></textarea>
+                        <div class="mb-3">
+                            <textarea class="form-control" style="height: 100px" name="description" placeholder="Description"></textarea>
+                        </div>
+                        <div class="d-flex mb-3">  
+                            <div class="form-check form-switch me-3">
+                                <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" name='status' checked>
+                                <label class="form-check-label" for="flexSwitchCheckChecked">Status</label>
                             </div>
-                        </div>  
-                        <div class="form-check form-switch">
-                            <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" name='status' checked>
-                            <label class="form-check-label" for="flexSwitchCheckChecked">Status</label>
+                            <div class="form-check form-switch">
+                                <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" name='popular'>
+                                <label class="form-check-label" for="flexSwitchCheckChecked">Trending</label>
+                            </div>
                         </div>
-                        <div class="form-check form-switch">
-                            <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" name='popular'>
-                            <label class="form-check-label" for="flexSwitchCheckChecked">Trending</label>
-                        </div>
-                        
-                        <div class="col-sm-10">
-                            <button type="submit" class="btn btn-primary">Submit Form</button>
-                        </div>
+                        <button type="submit" class="btn btn-primary w-100 btn-submit">Submit Form</button>
                     </form>
                 </div>
             </div>
         </div>
       </div>
  </section>
+
+ <style>
+    input.form-control:focus, textarea.form-control:focus {
+        outline: none;
+        box-shadow: none;
+        border: 1px solid green;
+    }
+
+    input.form-control, textarea.form-control, .btn-submit {
+        border: 1px solid blue;
+        border-radius: 0px;
+    }
+ </style>
 
 @endsection
