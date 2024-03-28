c7fbf7ef9ff3f5fd520c8fe782d78cbcfef45bda
Al-Nazre <punked0033@gmail.com>
2024-03-29 00:47:58 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,16 @@ Route::post('/delete-cart-item/', [App\Http\Controllers\CartController::class, '
 
 Route::middleware(['auth', 'auth'])->group(function () {
     
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/test/', [App\Http\Controllers\HomeController::class, 'testUi']);
+Route::get('/category/{slug}', [App\Http\Controllers\HomeController::class, 'subjectView'])->name('category');
+Route::get('/category-books/{slug}', [App\Http\Controllers\HomeController::class, 'subjectBooks'])->name('subjectBooks');
+Route::get('/category-books/{subject_slug}/{sub_subject_slug}', [App\Http\Controllers\HomeController::class, 'subSubjectBooks'])->name('subSubjectBooks');
+Route::get('/book/{slug}', [App\Http\Controllers\HomeController::class, 'bookDetail'])->name('bookDetail');
+Route::post('/add-to-cart/', [App\Http\Controllers\CartController::class, 'addToCart']);
+Route::get('/cart/', [App\Http\Controllers\CartController::class, 'index'])->name('cart');
+Route::post('/delete-cart-item/', [App\Http\Controllers\CartController::class, 'deletItem']);
+
 });
     
 
