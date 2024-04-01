830937f98c64593dd5d1d54099607e59a149b7a9
Syed Shaon <53667342+OrionShaon@users.noreply.github.com>
2024-04-01 22:32:14 +0600

    Update add.blade.php

--- resources/views/admin/book/add.blade.php
+++ resources/views/admin/book/add.blade.php
@@ -10,109 +10,114 @@
                     <div class="row">
                         <div class="col-lg-6">
                             @csrf
-                            <div class="row mb-3">
-                                <label class="col-sm-2 col-form-label">Subject</label>
-                                <div class="col-sm-10">
-                                    <select class="form-select" aria-label="Default select example" name='subject_id'>
+                            
+                            <div class="mb-4 form-outline">
+                                <label for="name" class="mb-2 fw-bold">Subject:</label>
+                                <select class="form-select" aria-label="Default select example" name='subject_id'>
                                     <option selected>Select Subject</option>
 
                                     @foreach ($subjects as $subject)
                                     <option value="{{ $subject->id }}">{{ $subject->name }}</option>
                                     @endforeach
                                     
-                                    </select>
-                                </div>
+                                </select>
                             </div>
-                            <div class="row mb-3">
-                                <label class="col-sm-2 col-form-label">Sub-Subject</label>
-                                <div class="col-sm-10">
-                                    <select class="form-select" aria-label="Default select example" name='sub_subject_id'>
+                            
+                            <div class="mb-4 form-outline">
+                                <label for="name" class="mb-2 fw-bold">Sub-Subject:</label>
+                                <select class="form-select" aria-label="Default select example" name='sub_subject_id'>
                                     <option selected>Select Sub-Subject</option>
 
                                     @foreach ($subSubjects as $subject)
                                     <option value="{{ $subject->id }}">{{ $subject->name }}</option>
                                     @endforeach
-                                    
-                                    </select>
-                                </div>
                             </div>
-                            <div class="row mb-3">
-                                <label class="col-sm-2 col-form-label">Author</label>
-                                <div class="col-sm-10">
-                                    <select class="form-select" aria-label="Default select example" name='author_id'>
+
+                            <div class="mb-4 form-outline">
+                                <label for="name" class="mb-2 fw-bold">Author:</label>
+                                <select class="form-select" aria-label="Default select example" name='author_id'>
                                     <option selected>Select Author</option>
 
                                     @foreach ($authors as $author)
                                     <option value="{{ $author->id }}">{{ $author->name }}</option>
                                     @endforeach
                                     
-                                    </select>
-                                </div>
+                                </select>
                             </div>
-                            <div class="row mb-3">
-                                <label class="col-sm-2 col-form-label">Publication</label>
-                                <div class="col-sm-10">
-                                    <select class="form-select" aria-label="Default select example" name='publication_id'>
+                            
+
+                            <div class="mb-4 form-outline">
+                                <label for="name" class="mb-2 fw-bold">Publication:</label>
+                                <select class="form-select" aria-label="Default select example" name='publication_id'>
                                     <option selected>Open Subject</option>
 
                                     @foreach ($publications as $publication)
                                     <option value="{{ $publication->id }}">{{ $publication->name }}</option>
                                     @endforeach
                                     
-                                    </select>
-                                </div>
+                                </select>
                             </div>
-                            <div class="row mb-3">
-                                <label class="col-sm-2 col-form-label">Book Type</label>
-                                <div class="col-sm-10">
-                                    <select class="form-select" aria-label="Default select example" name='type_id'>
+                            
+                            <div class="mb-4 form-outline">
+                                <label for="name" class="mb-2 fw-bold">Book Type:</label>
+                                <select class="form-select" aria-label="Default select example" name='type_id'>
                                     <option selected>Open book Type</option>
 
                                     @foreach ($types as $type)
                                     <option value="{{ $type->id }}">{{ $type->name }}</option>
                                     @endforeach
                                     
-                                    </select>
-                                </div>
+                                </select>
                             </div>
-                            <div class="mb-3 form-outline">
+                            <div class="mb-4 form-outline">
+                                <label for="name" class="mb-2 fw-bold">Name:</label>
                                 <input type="text" class="form-control " name ='name' placeholder="Name">
                             </div>
                             <div class="mb-3">
+                                <label for="Slug" class="mb-2 fw-bold">Slug:</label>
                                 <input type="text" class="form-control" name ='slug' placeholder ='Slug'>
                             </div>
                             <div class="mb-3">
+                                <label for="name" class="mb-2 fw-bold">Code:</label>
                                 <input type="text" class="form-control" name ='code' placeholder ='Code'>
                             </div>
                             <div class="mb-3">
+                                <label for="image" class="mb-2 fw-bold">Image:</label>
                                 <input class="form-control" type="file" id="formFile" name="img" placeholder="Image">
                             </div>
                             
                             <div class="mb-3">
+                                <label for="short_description" class="mb-2 fw-bold">Short Description:</label>
                                 <textarea class="form-control" style="height: 100px" name="short_description" placeholder="Short Description"></textarea>
                             </div>
                             <div class="mb-3">
+                                <label for="short_description" class="mb-2 fw-bold">Description:</label>
                                 <textarea class="form-control" style="height: 100px" name="description" placeholder="Description"></textarea>
                             </div>
-                            <div class="mb-3">
-                                <input type="number" step="any" class="form-control" name ='price' placeholder ='Price'>
-                            </div>
-                            <div class="mb-3">
-                                <input type="number" step="any" class="form-control" name ='discount' placeholder ='Discount'>
-                            </div>
-                            <div class="mb-3">
-                                <input type="number" class="form-control" name ='qty' placeholder ='Quantity'>
-                            </div>
-                            
                         </div>
                         <div class="col-lg-6">
                             <div class="mb-3">
+                                <label for="Price" class="mb-2 fw-bold">Price:</label>
+                                <input type="number" step="any" class="form-control" name ='price' placeholder ='Price'>
+                            </div>
+                            <div class="mb-3">
+                                <label for="Discount" class="mb-2 fw-bold">Discount:</label>
+                                <input type="number" step="any" class="form-control" name ='discount' placeholder ='Discount'>
+                            </div>
+                            <div class="mb-3">
+                                <label for="Quantity" class="mb-2 fw-bold">Quantity:</label>
+                                <input type="number" class="form-control" name ='qty' placeholder ='Quantity'>
+                            </div>
+                            <div class="mb-3">
+                                <label for="Meta_Title" class="mb-2 fw-bold">Meta Title:</label>
                                 <input type="text" class="form-control" name ='meta_title' placeholder ='Meta Title'>
                             </div>
                             <div class="mb-3">
+                                <label for="meta_keywords" class="mb-2 fw-bold">Meta Keywords:</label>
                                 <input type="text" class="form-control" name ='meta_keywords' placeholder ='Meta Keywords'>
                             </div>
                             <div class="mb-3">
+                                <label for="meta_description" class="mb-2 fw-bold">Meta Description:</label>
                                 <textarea class="form-control" style="height: 80px" name="meta_description" placeholder="Meta Description"></textarea>
                             </div>
                             
@@ -127,15 +132,19 @@
                                 </div>
                             </div>
                             <div class="mb-3">
+                                <label for="Edition" class="mb-2 fw-bold">Edition:</label>
                                 <input type="text" class="form-control" name ='edition' placeholder ='Edition'>
                             </div>
                             <div class="mb-3">
+                                <label for="Publish" class="mb-2 fw-bold">Publish:</label>
                                 <input type="text" class="form-control" name ='publish' placeholder ='Publish'>
                             </div>
                             <div class="mb-3">
+                                <label for="Country" class="mb-2 fw-bold">Country:</label>
                                 <input type="text" class="form-control" name ='country' placeholder ='Country'>
                             </div>
                             <div class="mb-3">
+                                <label for="Language" class="mb-2 fw-bold">Language:</label>
                                 <input type="text" class="form-control" name ='language' placeholder ='Language'>
                             </div>
                             <button type="submit" class="btn btn-primary w-100 btn-submit">Submit Form</button>
