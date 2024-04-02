2da36adf2cd74312484c244c881e1312127a9674
Syed Shaon <53667342+OrionShaon@users.noreply.github.com>
2024-04-03 03:34:44 +0600

    mega menu

--- resources/views/layouts/inc/navbar.blade.php
+++ resources/views/layouts/inc/navbar.blade.php
@@ -1,11 +1,16 @@
 <div class="nav-area border-bottom border-light bg-white d-lg-block d-none">
     <div class="container">
-        <nav id="main-menu">
-            <ul class="d-flex align-items-center justify-content-center list-unstyled mb-0">
-                <li>
+        <nav id="main-menu" class="position-relative">
+            <ul class="d-flex align-items-center justify-content-center position-ststic list-unstyled mb-0 w-100">
+                <li class="position-static" id="home">
                     <a href="{{ route('home') }}">
                         <span>হোম</span>
                     </a>
+                    <div class="position-absolute bg-secondary rounded-2 rounded-bottom border-top border-primary border-4" id="home-Mega-menu" style="left: 0; right: 0;">
+                        <div class="p-4 text-center">
+                            <h3 class="display-3">Hello! :-)</h3>
+                        </div>
+                    </div>
                 </li>
                 <li>
                     <a href="./category.html">
