28aa01c068260993d5970e0ce56415f2a47e7d26
Syed Shaon <53667342+OrionShaon@users.noreply.github.com>
2024-04-04 03:00:11 +0600

    add megamenu column , button and shadow. updated color of megamenu

--- resources/views/layouts/inc/navbar.blade.php
+++ resources/views/layouts/inc/navbar.blade.php
@@ -1,5 +1,6 @@
 @php
-    
+    use App\Models\Subject;
+    $subjects = Subject::all();
 @endphp
 <div class="nav-area border-bottom border-light bg-white d-lg-block d-none">
     <div class="container">
@@ -30,10 +31,19 @@
                     <a href="{{ route('category') }}">
                         <span>বিষয়</span>
                     </a>
-                    <div class="position-absolute bg-primary rounded-2 rounded-bottom border-top border-primary border-4" id="home-Mega-menu" style="left: 0; right: 0;">
-                        <div class="p-4 text-center">
-                            <a href=""><p></p></a>
-                            
+                    <div class="position-absolute bg-megamenu shadow-sm rounded-2 rounded-bottom border-4" id="home-Mega-menu" style="left: 0; right: 0;">
+                        <div class="p-4">
+                            <div class="row text-center mb-3">
+                                @foreach ($subjects as $subject)
+                                <div class="col-sm-3 col-md-4 col-lg-2">
+                                        <a href=""><p>{{ $subject->name }}</p></a>
+                                </div>
+                                @endforeach
+
+                            </div>
+                            <div class="w-100 d-flex justify-content-center">
+                                <button class="btn btn-outline-primary border-2 border-primary text-center megamenu-btn"  action= "">See More</button>
+                            </div>
                         </div>
                     </div>
                 </li>
