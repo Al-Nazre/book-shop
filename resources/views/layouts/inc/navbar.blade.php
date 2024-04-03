2ee1a5a45e77906fadf7039ee3b6444943c39764
Al-Nazre <punked0033@gmail.com>
2024-04-04 03:20:03 +0600

    Update navbar.blade.php

--- resources/views/layouts/inc/navbar.blade.php
+++ resources/views/layouts/inc/navbar.blade.php
@@ -1,21 +1,19 @@
 @php
     use App\Models\Subject;
+    use App\Models\Author;
+    use App\Models\Publication;
     $subjects = Subject::all();
+    $authors = Author::all();
+    $publications = Publication::all();
 @endphp
 <div class="nav-area border-bottom border-light bg-white d-lg-block d-none">
     <div class="container">
         <nav id="main-menu" class="position-relative">
             <ul class="d-flex align-items-center justify-content-center position-ststic list-unstyled mb-0 w-100">
-                <li class="position-static" id="home">
+                <li>
                     <a href="{{ route('home') }}">
                         <span>হোম</span>
                     </a>
-                    <div class="position-absolute bg-primary rounded-2 rounded-bottom border-top border-primary border-4" id="home-Mega-menu" style="left: 0; right: 0;">
-                        <div class="p-4 text-center">
-                            <a href=""><p></p></a>
-                            
-                        </div>
-                    </div>
                 </li>
                 <li>
                     <a href="">
@@ -34,9 +32,9 @@
                     <div class="position-absolute bg-megamenu shadow-sm rounded-2 rounded-bottom border-4" id="home-Mega-menu" style="left: 0; right: 0;">
                         <div class="p-4">
                             <div class="row text-center mb-3">
-                                @foreach ($subjects as $subject)
+                                @foreach ($subjects as $item)
                                 <div class="col-sm-3 col-md-4 col-lg-2">
-                                        <a href=""><p>{{ $subject->name }}</p></a>
+                                        <a href=""><p>{{ $item->name }}</p></a>
                                 </div>
                                 @endforeach
 
@@ -47,22 +45,50 @@
                         </div>
                     </div>
                 </li>
+
                 <li class="position-static" id="home">
                     <a href="">
                         <span>লেখক</span>
                     </a>
-                    <div class="position-absolute bg-primary rounded-2 rounded-bottom border-top border-primary border-4" id="home-Mega-menu" style="left: 0; right: 0;">
-                        <div class="p-4 text-center">
-                            <a href=""><p></p></a>
-                            
+                    <div class="position-absolute bg-megamenu shadow-sm rounded-2 rounded-bottom border-4" id="home-Mega-menu" style="left: 0; right: 0;">
+                        <div class="p-4">
+                            <div class="row text-center mb-3">
+                                @foreach ( $authors as $item)
+                                <div class="col-sm-3 col-md-4 col-lg-2">
+                                        <a href=""><p>{{ $item->name }}</p></a>
+                                </div>
+                                @endforeach
+
+                            </div>
+                            <div class="w-100 d-flex justify-content-center">
+                                <button class="btn btn-outline-primary border-2 border-primary text-center megamenu-btn"  action= "">See More</button>
+                            </div>
                         </div>
                     </div>
                 </li>
-                <li>
+
+                <li class="position-static" id="home">
                     <a href="">
-                        <span>প্রকাশক</span>
+                        <span>প্রকাশনি</span>
                     </a>
+                    <div class="position-absolute bg-megamenu shadow-sm rounded-2 rounded-bottom border-4" id="home-Mega-menu" style="left: 0; right: 0;">
+                        <div class="p-4">
+                            <div class="row text-center mb-3">
+                                @foreach ($publications as $item)
+                                <div class="col-sm-3 col-md-4 col-lg-2">
+                                        <a href=""><p>{{ $item->name }}</p></a>
+                                </div>
+                                @endforeach
+
+                            </div>
+                            <div class="w-100 d-flex justify-content-center">
+                                <button class="btn btn-outline-primary border-2 border-primary text-center megamenu-btn"  action= "">See More</button>
+                            </div>
+                        </div>
+                    </div>
                 </li>
+               
+                
                 
                 <li>
                     <a href="">
